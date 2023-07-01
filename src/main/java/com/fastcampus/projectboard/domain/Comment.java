ac7eb13e9746c02876957ebc45b8bb11b8893334
moonbyeongmin <qudalsrnt3x@gmail.com>
2023-07-02 01:14:38 +0900

    #10 - 도메인 코드를 엔티티로 변환
    1 도메인 클래스 == 1 테이블 매칭되도록 설계
    생성자와 'equals()', 'hashcode()' 등이 도메인 맥락에 맞게 설정

--- src/main/java/com/fastcampus/projectboard/domain/Comment.java
+++ src/main/java/com/fastcampus/projectboard/domain/Comment.java
@@ -1,14 +1,62 @@
 package com.fastcampus.projectboard.domain;
 
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class Comment {
-    private Long id;
-    private Article article; // 게시글 (ID)
-    private String content; // 본문
 
-    private LocalDateTime createdAt; // 생성일시
-    private String createdBy; // 생성자
-    private LocalDateTime modifiedAt; // 수정일시
-    private String modifiedBy; // 수정자
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter @ManyToOne(optional = false) private Article article; // 게시글 (ID)
+    @Setter @Column(nullable = false, length = 500) private String content; // 본문
+
+    @CreatedDate
+    @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
+    @CreatedBy
+    @Column(nullable = false, length = 100) private String createdBy; // 생성자
+    @LastModifiedDate
+    @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일시
+    @LastModifiedBy
+    @Column(nullable = false, length = 100) private String modifiedBy; // 수정자
+
+    private Comment(Article article, String content) {
+        this.article = article;
+        this.content = content;
+    }
+
+    public static Comment of(Article article, String content) {
+        return new Comment(article, content);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Comment comment = (Comment) o;
+        return id != null && id.equals(comment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
