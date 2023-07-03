64e7d6d70177d8896629835ac65f07c6a0f7eb0e
qudalsrnt3x <qudalsrnt3x@entwiz.com>
2023-07-03 14:23:06 +0900

    #1 - Querydsl 지원을 이용하여 api 검색 옵션 추가
    'QuerydslPredicateExecutor', 'QuerydslPredicateExecutor' 이용하여 간단하게 개발

--- src/main/java/com/fastcampus/projectboard/repository/CommentRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/CommentRepository.java
@@ -1,9 +1,27 @@
 package com.fastcampus.projectboard.repository;
 
 import com.fastcampus.projectboard.domain.Comment;
+import com.fastcampus.projectboard.domain.QComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+public interface CommentRepository extends
+        JpaRepository<Comment, Long>,
+        QuerydslPredicateExecutor<Comment>,
+        QuerydslBinderCustomizer<QComment> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QComment root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.createdAt, root.createdBy, root.content);
+        // bindings.bind(root.title).first(StringExpression::likeIgnoreCase); // like '${v}'
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
