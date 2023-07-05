61fdaa8afb5a911eaae2d5652e872acd8de75505
qudalsrnt3x <qudalsrnt3x@entwiz.com>
2023-07-05 10:46:51 +0900

    #20 - 댓글 기능 구현 및 전체 테스트 동작 확인
    강의 중 댓글 기능의 테스트를 구현하지 않았었는데
    이를 마저 구현
    전체 테스트 통과 확인

--- src/main/java/com/fastcampus/projectboard/service/CommentService.java
+++ src/main/java/com/fastcampus/projectboard/service/CommentService.java
@@ -1,14 +1,19 @@
 package com.fastcampus.projectboard.service;
 
+import com.fastcampus.projectboard.domain.Article;
+import com.fastcampus.projectboard.domain.Comment;
 import com.fastcampus.projectboard.dto.CommentDto;
 import com.fastcampus.projectboard.repository.ArticleRepository;
 import com.fastcampus.projectboard.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Transactional
 @Service
@@ -19,18 +24,28 @@ public class CommentService {
 
     @Transactional(readOnly = true)
     public List<CommentDto> searchArticleComment(Long articleId) {
-        return List.of();
+        return commentRepository.findByArticle_Id(articleId).stream().map(CommentDto::from).toList();
     }
 
     public void saveComment(CommentDto dto) {
-
+        try {
+            Article article = articleRepository.getReferenceById(dto.articleId());
+            commentRepository.save(dto.toEntity(article));
+        } catch (EntityNotFoundException e) {
+            log.warn("댓글 저장 실패. 댓글의 게시글을 찾을 수 없습니다. - dto {}", dto);
+        }
     }
 
     public void updateComment(CommentDto dto) {
-
+        try {
+            Comment comment = commentRepository.getReferenceById(dto.id());
+            if (dto.content() != null) comment.setContent(dto.content());
+        } catch (EntityNotFoundException e) {
+            log.warn("댓글 업데이트 실패. 댓글을 찾을 수 없습니다 - dto: {}", dto);
+        }
     }
 
     public void deleteComment(Long commentId) {
-
+        commentRepository.deleteById(commentId);
     }
 }
